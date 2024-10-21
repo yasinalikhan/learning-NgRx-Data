50ec096415d2a475a9eaa85af6410bb8861e0fc7
Yasin Ali Khan <yasinalikhan55@gmail.com>
2024-10-21 12:34:44 +0530

    Setup new Project for Ngrx Data. Install all ngrx dependencies for ngrx data in Ngrx

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PostListComponent } from './post/post-list/post-list.component';
+import { AddPostComponent } from './post/add-post/add-post.component';
+import { EditPostComponent } from './post/edit-post/edit-post.component';
+import { SinglePostComponent } from './post/single-post/single-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:HomeComponent
+  },
+  {
+    path: 'posts',
+    component:PostListComponent
+  },
+  {
+    path:'posts/add',
+    component:AddPostComponent
+  },
+  {
+    path:'posts/edit/:id',
+    component:EditPostComponent
+  },
+  {
+    path:'post/details/:id',
+    component:SinglePostComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post/post-list/post-list.component';
+import { SinglePostComponent } from './post/single-post/single-post.component';
+import { AddPostComponent } from './post/add-post/add-post.component';
+import { EditPostComponent } from './post/edit-post/edit-post.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    SinglePostComponent,
+    AddPostComponent,
+    EditPostComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
